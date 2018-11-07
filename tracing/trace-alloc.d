cbb4e20da281cadf099bbb1c25a44b7d1eebb4a3
Lucian Paul-Trifu <lmp53@cam.ac.uk>
2018-11-07 16:22:00 +0000

    Only trace mmap/unmap by the allocator

--- tracing/trace-alloc.d
+++ tracing/trace-alloc.d
@@ -30,6 +30,7 @@ bool ustack_ids_initd;
 #define ustack_save(keys...) \
 	@ustack_ids[keys, ustack()] = min(1)
 
+self int in_allocator_std;
 
 BEGIN
 {
@@ -48,6 +49,10 @@ END
 pid$target:libdlmalloc*:malloc:entry
 {
 	self->malloc_arg0 = arg0;
+	self->in_allocator_std += 1;
+}
+pid$target:libdlmalloc*:malloc:return {
+	self->in_allocator_std -= 1;
 }
 pid$target:libdlmalloc*:malloc:return
 / arg1 && self->malloc_arg0 && !self->using_malloc / {
@@ -63,9 +68,11 @@ pid$target:libdlmalloc*:calloc:entry
 	self->calloc_arg0 = arg0;
 	self->calloc_arg1 = arg1;
 	self->using_malloc = 1;
+	self->in_allocator_std += 1;
 }
 pid$target:libdlmalloc*:calloc:return {
 	self->using_malloc = 0;
+	self->in_allocator_std -= 1;
 }
 pid$target:libdlmalloc*:calloc:return
 / arg1 && self->calloc_arg0 && self->calloc_arg1 / {
@@ -81,9 +88,11 @@ pid$target:libdlmalloc*:valloc:entry
 {
 	self->valloc_arg0 = arg0;
 	self->using_malloc = 1;
+	self->in_allocator_std += 1;
 }
 pid$target:libdlmalloc*:valloc:return {
 	self->using_malloc = 0;
+	self->in_allocator_std -= 1;
 }
 pid$target:libdlmalloc*:valloc:return
 / arg1 && self->valloc_arg0 / {
@@ -100,9 +109,11 @@ pid$target:libdlmalloc*:posix_memalign:entry
 	self->posix_memalign_arg1 = arg1;
 	self->posix_memalign_arg2 = arg2;
 	self->using_malloc = 1;
+	self->in_allocator_std += 1;
 }
 pid$target:libdlmalloc*:posix_memalign:return {
 	self->using_malloc = 0;
+	self->in_allocator_std -= 1;
 }
 pid$target:libdlmalloc*:posix_memalign:return
 / arg1 == 0 && self->posix_memalign_arg1 && self->posix_memalign_arg2 / {
@@ -123,10 +134,12 @@ pid$target:libdlmalloc*:realloc:entry
 	self->realloc_arg1 = arg1;
 	self->using_malloc = 1;
 	self->using_free = 1;
+	self->in_allocator_std += 1;
 }
 pid$target:libdlmalloc*:realloc:return {
 	self->using_malloc = 0;
 	self->using_free = 0;
+	self->in_allocator_std -= 1;
 }
 pid$target:libdlmalloc*:realloc:return
 / (self->realloc_arg0 || self->realloc_arg1) &&
@@ -151,7 +164,8 @@ pid$target::mmap:entry
 	self->mmap_arg5 = arg5;
 }
 pid$target::mmap:return
-/ (void*)arg1 != (void*)-1 && self->mmap_arg1 && (self->mmap_arg3 & 0x1000) / {
+/ (void*)arg1 != (void*)-1 && self->mmap_arg1 && (self->mmap_arg3 & 0x1000)
+  && self->in_allocator_std > 0 / {
 	this->ts = timestamp;
 	/* ustack_save(tid, this->ts); */
 	printf("\n%d %s(%p, %d, %x, %x, %d, %x): %p %d", this->ts, probefunc,
@@ -175,7 +189,7 @@ pid$target::munmap:entry
 	self->munmap_arg1 = arg1;  /* length */
 }
 pid$target::munmap:return
-/ !arg1 && self->munmap_arg1 / {
+/ !arg1 && self->munmap_arg1 && self->in_allocator_std > 0 / {
 	this->ts = timestamp;
 	/* ustack_save(tid, this->ts); */
 	printf("\n%d %s(%p, %d): %p %d", this->ts, probefunc,
