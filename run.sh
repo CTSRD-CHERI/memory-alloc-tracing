e30d3b8b684b171fec0424deb74cdc7308052ca8
Lucian Paul-Trifu <lmp53@cam.ac.uk>
2019-04-10 16:11:14 +0100

    fixup! SIGUSR1 the workload driver rather than the traced workload process
    Do not try to
    $ wait $workload_pid
    as it is not a child process.

--- run.sh
+++ run.sh
@@ -130,7 +130,7 @@ do
 done && mv $samples_file-processing $samples_file ;} &
 
 # Post-process
-wait $COPROC_PID $workload_pid
+wait $COPROC_PID
 wait $dtrace_pid
 test -d ${run_dir} &&
   $my_dir/tracing/post/merge-samples-and-trace.sh $samples_file $trace_file >${run_info_file} &&
